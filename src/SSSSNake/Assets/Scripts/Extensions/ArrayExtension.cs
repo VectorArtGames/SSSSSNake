a48bf597b4b73c7cfb3e8081363a375ace9502b4
VectorArtGames <70201838+VectorArtGames@users.noreply.github.com>
2020-09-14 22:19:25 +0200

    noted

---
+++ src/SSSSNake/Assets/Scripts/Extensions/ArrayExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Extensions
+{
+	public static class ArrayExtension
+	{
+		public static void Shift<T>(this T[] array, int by, T value)
+		{
+			var arr = array.Skip(by).ToArray();
+			arr.CopyTo(array, 0);
+		}
+	}
+}
--- src/SSSSNake/Assets/Scripts/Player/SnakeCore.cs
+++ src/SSSSNake/Assets/Scripts/Player/SnakeCore.cs
@@ -1,35 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Extensions;
+using Assets.Scripts.Player.Tails;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SnakeCore : MonoBehaviour
 {
-    [SerializeField]
-    private int _length = 1;
-    public int Length
+	public List<Tail> tails = new List<Tail>();
+
+	public void SpawnTails()
 	{
-        get => _length;
-        set => _length = value;
+
 	}
 
-    private Vector2 _position;
-    public Vector2 Position
-	{
-        get => _position;
-        set
-		{
-            Reposition(value);
-            _position = value;
-		}
-	}
 
-	public void Awake()
-	{
-		Reposition(transform.position);
-	}
-
-	public void Reposition(Vector2 value)
-	{
-        transform.position = new Vector2((int)value.x, (int)value.y);
-	}
 }
