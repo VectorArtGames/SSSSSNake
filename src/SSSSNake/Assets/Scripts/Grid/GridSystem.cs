0f0cfbb8dd91a87aecee27a8278ac33b44889560
VectorArtGames <70201838+VectorArtGames@users.noreply.github.com>
2020-09-14 13:28:59 +0200

    asd

---
+++ src/SSSSNake/Assets/Scripts/Grid/GridSystem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Grid.Attach;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+	public Gridable[,] Gridables { get; set; }
+}
---
+++ src/SSSSNake/Assets/Scripts/Grid/Interfaces/IGridable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Grid.Interfaces
+{
+	public interface IGridable
+	{
+		int X { get; set; }
+		int Y { get; set; }
+		GameObject AttachedObject { get; set; }
+		Vector2 Position { get; set; }
+	}
+}
