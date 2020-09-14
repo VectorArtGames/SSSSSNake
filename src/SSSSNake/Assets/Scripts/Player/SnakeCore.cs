82327a25666de411c4bc79bae2434f95d21e8e40
ADEL\spp109 <martin.magnusson@adelfors.nu>
2020-09-14 14:31:59 +0200

    Removed annoying things

---
+++ src/SSSSNake/Assets/Scripts/Player/SnakeCore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeCore : MonoBehaviour
+{
+    [SerializeField]
+    private int _length = 1;
+    public int Length
+	{
+        get => _length;
+        set => _length = value;
+	}
+
+    private Vector2 _position;
+    public Vector2 Position
+	{
+        get => _position;
+        set
+		{
+            Reposition(value);
+            _position = value;
+		}
+	}
+
+	public void Awake()
+	{
+		Reposition(transform.position);
+	}
+
+	public void Reposition(Vector2 value)
+	{
+        transform.position = new Vector2((int)value.x, (int)value.y);
+	}
+}
