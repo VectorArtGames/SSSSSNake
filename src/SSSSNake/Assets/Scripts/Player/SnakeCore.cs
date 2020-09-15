5fb06864600200ef24d2e354774c63627ed3a5e2
ADEL\spp109 <martin.magnusson@adelfors.nu>
2020-09-15 11:17:06 +0200

    Made the design of interface

--- src/SSSSNake/Assets/Scripts/Player/SnakeCore.cs
+++ src/SSSSNake/Assets/Scripts/Player/SnakeCore.cs
@@ -1,20 +1,51 @@
-﻿using System;
+﻿using Assets.Scripts.Player.Tails;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Assets.Scripts.Extensions;
-using Assets.Scripts.Player.Tails;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class SnakeCore : MonoBehaviour
 {
-	public List<Tail> tails = new List<Tail>();
+	public Transform Border_L;
+	public Rect debug_rect;
 
-	public void SpawnTails()
+	public GameObject[,] Grid;
+	public int Vertical, Horizontal;
+	public int Rows, Columns;
+
+	private void Awake()
 	{
-
+		var rect = Border_L.GetComponent<RectTransform>();
+		//rect.rect.position = rect.anchoredPosition;
 	}
 
+	public void Start()
+	{
+		var b = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
 
+		Debug.Log(b);
+
+		Vertical = (int)Camera.main.orthographicSize;
+		Horizontal = Vertical * (Screen.width / Screen.height);
+		Columns = Horizontal * 2;
+		Rows = Vertical * 2;
+		Grid = new GameObject[Columns, Rows];
+		for (var i = 0; i < Columns; i++)
+		{
+			for (var j = 0; j < Rows; j++)
+			{
+				SpawnTile(i, j, ref Grid[i, j]);
+			}
+		}
+	}
+
+	private void SpawnTile(int x, int y, ref GameObject value)
+	{
+		var obj = new GameObject($"Tile_{x}_{y}");
+		obj.AddComponent<Tail>();
+		obj.transform.position = new Vector2(x - (Horizontal - 0.5f), y - (Vertical - 0.5f));
+		value = obj;
+	}
 }
