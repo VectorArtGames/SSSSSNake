ebd0c2b595a4fbadd6b4a9f30f6c849b477bb110
ADEL\spp109 <martin.magnusson@adelfors.nu>
2020-09-30 15:05:50 +0200

    Fixed a lot of things.
    [Planning] - Added Notes.txt as well as dialogs

---
+++ src/SSSSNake/Assets/Game/Scripts/Global/Scores/ScoreTracker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Events;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScoreTracker : MonoBehaviour
+{
+	#region Singleton
+
+	public static ScoreTracker Instance { get; set; }
+
+	public void Awake()
+	{
+		Instance = this;
+	}
+
+	#endregion
+
+
+	[Header("On Score Updated")]
+	public UnityEvent eventHandle;
+
+	[Header("Text"),Space(5)]
+	public TextMeshProUGUI[] texts;
+
+	[SerializeField]
+	private long score;
+	public long Score
+	{
+		get => score;
+		set
+		{
+			if (value > int.MaxValue) return;
+			score = value;
+			eventHandle.Invoke();
+		}
+	}
+
+	public void Restart()
+	{
+		Score = 0;
+	}
+
+	private void Start()
+	{
+		Score = 0;
+	}
+
+	public void UpdateText()
+	{
+		foreach (var obj in texts)
+		{
+			obj.text = Score.ToString("00000000");
+		}
+	}
+
+	public static void SetHighscore(int value) =>
+		PlayerPrefs.SetInt("Highscore", value);
+
+	public static long GetHighscore() => 
+		PlayerPrefs.GetInt("Highscore");
+}
---
+++ src/SSSSNake/Assets/Game/Scripts/UI/DeathScreen.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+	#region Singleton
+
+	public static DeathScreen Instance { get; set; }
+	public void Awake()
+	{
+		Instance = this;
+	}
+
+	#endregion
+
+	[Header("Required"), Space(5.0f)]
+	public GameObject GameOverScreen;
+	public GameObject GameScreen;
+	public TextMeshProUGUI ScoreText;
+
+	private SnakeCore PlayerCore;
+	private ScoreTracker scoreTracker;
+
+	public DeathScreenState State = DeathScreenState.None;
+
+	private void Start()
+	{
+		scoreTracker = ScoreTracker.Instance;
+		PlayerCore = SnakeCore.Instance;
+	}
+
+	public void OnDeath()
+	{
+		GameScreen.SetActive(false); // disable game
+		GameOverScreen.SetActive(true); // enable death screen
+		Time.timeScale = 0; // Freeze time
+		if (ScoreText == null || scoreTracker == null)
+		{
+			#if UNITY_EDITOR
+				Debug.LogError("Unexpected error occurred!\nDeathScreen");
+			#endif
+			return;
+		}
+
+		/* Highscore - Get highscore.
+		 * compare with current score,
+		 * if higher than current score..
+		 * Set highscore, if lower.. Do nothing.
+		 */
+		var score = ScoreTracker.GetHighscore();
+		if (scoreTracker.Score > score)
+			ScoreTracker.SetHighscore(Convert.ToInt32(scoreTracker.Score));
+
+		// Set highscore
+		ScoreText.text = $@"     Game Over.
+Score    : {scoreTracker.Score:00000000}
+Highscore: {ScoreTracker.GetHighscore():00000000}";
+
+
+		State = DeathScreenState.Dead;
+
+	}
+
+	private void Update()
+	{
+		if (State != DeathScreenState.Dead) return;
+		if (Input.GetKeyUp(KeyCode.KeypadEnter) 
+			|| Input.GetKeyUp(KeyCode.Space) 
+			|| Input.GetKeyUp(KeyCode.Return)
+			|| Input.GetKeyUp(KeyCode.R)) Restart();
+		else if (Input.GetKeyUp(KeyCode.Escape)) ExitToMainMenu();
+	}
+
+	public void Restart()
+	{
+		if (PlayerCore == null) return;
+		PlayerCore.Restart();
+		GameOverScreen.SetActive(false);
+		GameScreen.SetActive(true);
+		Time.timeScale = 1;
+		State = DeathScreenState.Game;
+	}
+
+	public void ExitToMainMenu()
+	{
+		SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+		Time.timeScale = 1;
+	}
+
+	public enum DeathScreenState
+	{
+		None,
+		Dead,
+		Game
+	}
+}
